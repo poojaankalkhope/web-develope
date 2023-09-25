4b5e74ea4856da4caeb5b60a8502bae82ddabd20
Sumit Parathnale <parathnalesumit@gmail.com>
2023-09-25 18:15:14 +0530

    javascript:variables

---
+++ day-24/data-types.js
@@ -0,0 +1,12 @@
+const phone = '1245678' // string
+const email = 'aa@bb.com'// string
+const isOfficial = false // boolean
+const age = 23 //number
+const address = { // object
+    lane: 'first lane', // key : value, key:value
+    street : 'second street',
+    shop: 4
+} // object
+const idProofs = [ 'pan', 'aadhar', 'lic' ] // array of strings , arrya of number, arrya of objects etc.
+
+const fn = function () {  } // function
---
+++ day-24/special-value.js
@@ -0,0 +1,11 @@
+let a;         
+console.log(a)  // undefined
+
+let b = null
+console.log(b)  // null
+
+let c = 10/'abc' 
+console.log(c)  // NaN
+
+let bl = !''
+console.log(bl) // NaN
