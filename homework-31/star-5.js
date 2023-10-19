abe058c24f52c87fff3a36f67a73242364535b8a
Sumit Parathnale <parathnalesumit@gmail.com>
2023-10-19 12:12:55 +0530

    star pattern printing

---
+++ homework-31/star-5.js
@@ -0,0 +1,17 @@
+for(let i=1 ; i<=5; i++)
+{
+    let str = '';
+    
+    for(let j=1; j<=5; j++) 
+    {
+        
+        if(j<=6-i) {
+            str += '*';
+        }
+        else {
+            str += ' ';
+        }
+        
+    }
+    console.log(str);
+}
---
+++ homework-31/star-7.js
@@ -0,0 +1,12 @@
+for(let i=1 ; i<=5; i++){
+    let str = '';
+   for(let j=1; j<=5; j++) {
+       if(j>=i) {
+           str += '*';
+       }
+       else {
+           str += ' ';
+       }
+   }
+   console.log(str);
+}
