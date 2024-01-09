1a01bad8d51d74632d50ff1954fa89cff467dc97
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-09 19:02:06 +0530

    revision of javascript

---
+++ day-50 R/app-01.js
@@ -0,0 +1,34 @@
+var num1=10
+num1=20
+
+var num2=20
+num2=30
+console.log(num1,num2)
+ var result = num1 + num2
+ console.log(result)
+
+
+ var nm=123 // number
+ var abc=24.56 // decimal number
+
+ var mk='abc' // string
+ var mn= "1234" // string
+
+ var isdb = true // boolean
+
+
+ console.log('object')
+ var dob = new Date() // object
+console.log(dob)
+
+var car={
+    nm:'abc',
+    mk:"xyz",
+    model: 2018,
+    isEv: true
+
+}
+console.log(car)
+
+var obj={abc:1, xyz:2, pqr:3}
+console.log(obj)
