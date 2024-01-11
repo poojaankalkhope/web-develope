56fee8e04c35bf621cda04c248996dfc218d0aa4
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-11 18:20:16 +0530

    function

---
+++ day-51/app-01.js
@@ -0,0 +1,28 @@
+var a= 20
+var b="10"
+c=a+b
+console.log(c)
+
+function sayHi() {
+    console.log("Hi how are you?")
+}
+
+sayHi()
+
+var a=10
+var sq1= a*a
+console.log(sq1)
+
+var b=20
+var sq2=b*b
+console.log(sq2)
+
+function sqr(n){
+    return n*n
+}
+
+var sq3= sqr(10)
+console.log(sq3)
+
+var sq4 = sqr(20)
+console.log(sq4)
---
+++ day-51/app-02.js
@@ -0,0 +1,15 @@
+// calculate the percentage
+
+var mk= 60
+var tmk = 100
+var prct = (mk / tmk) * 100
+console.log(prct)
+
+
+function calcper(mk, tmk) {
+    var per = (mk/ tmk) *100;
+    return per
+}
+
+var totalper= calcper(53 , 60)
+console.log(totalper)
