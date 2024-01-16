ab55936d344a10c9e337abf4837db8e9516eb483
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-16 12:59:07 +0530

    javascript: def oth mtd

--- day-40/oth-mtd-02.js
+++ day-40/oth-mtd-02.js
@@ -3,14 +3,14 @@ const book = {
     author: 'abc'
 }
 
- // console.log(Object.isSealed(book))
+  console.log(Object.isSealed(book))
  console.log(Object.isFrozen(book))
- // Object.seal(book)
+  Object.seal(book)
  Object.freeze(book)
  book.isbn = '89789'
  book.pages = 44
  delete book.pages
  console.log(book)
 
- //console.log(Object.isSealed(book))
+ console.log(Object.isSealed(book))
  console.log(Object.isFrozen(book))
