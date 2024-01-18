d47bf70823e6cd16b07a4533a229430a5b4254a5
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-18 11:33:48 +0530

    js : operators

---
+++ day-54/app-o1.js
@@ -0,0 +1,15 @@
+c=1
+//c=c+1
+//console.log(c)
+//console.log("Inc " + (c++) ) // c = c + 1
+//console.log(c)
+
+console.log("Inc " + (c++) ) // c = c + 1
+console.log("C "+ c)
+console.log("Dec " + (c--) ) // c = c - 1
+console.log("C "+ c)
+console.log('C' + (++c))
+console.log('C' + (--c))
+console.log('C' + (c++))
+console.log("C"+ c)
+
