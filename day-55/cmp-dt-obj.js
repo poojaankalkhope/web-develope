3d0ea4eee513d809baca9be27e389441ff940f83
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-23 13:44:35 +0530

    js :how compare dt obj

---
+++ day-55/cmp-dt-obj.js
@@ -0,0 +1,10 @@
+let date1= new Date(2023-22-1)
+let date2= new Date(2023-22-1)
+if(date1 < date2){
+    console.log(`${date1} is greater than  ${date2}`)
+} else if(date1 > date2) {
+    console.log(`${date1} is less than  ${date2}`)
+} else {
+    console.log(`${date1} is equal to  ${date2}`)
+}
+ 
