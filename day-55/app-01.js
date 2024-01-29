e559e3d0d1f53f3efbdbe7f1b110fe502198e76b
Sumit Parathnale <parathnalesumit@gmail.com>
2024-01-29 17:55:41 +0530

    index-number

--- day-55/app-01.js
+++ day-55/app-01.js
@@ -1,16 +1,16 @@
 const arr =[1,3,30,10,2,89,45,32,90,67]
 
-const number=100
+const number=30
 
 const index=arr.indexOf(number)
  
 function findIndex(arr1 ,index) {
    
      if(index !== -1){
-        console.log(`${number}  Number is available`)
+        console.log(`${number}  Number is available at ${index} position`)
     }  
     else{
-       // console.log(` ${number} Number is not available`)
+      
       console.log(`${index}`)
     }
 }
