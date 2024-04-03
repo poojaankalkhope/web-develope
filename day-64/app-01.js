092c789f7dcf75a8561d1fee969b5207c36e32ff
Sumit Parathnale <parathnalesumit@gmail.com>
2024-04-03 15:08:27 +0530

    dom : query selector

---
+++ day-64/app-01.js
@@ -0,0 +1,11 @@
+document.addEventListener( 'DOMContentLoaded',() => {
+
+   //const el = document.querySelector('body :nth-child(3)')
+
+   //const el = document.querySelector('#p1')
+
+   const el = document.querySelector('.ing')
+
+   console.log(el.innerText)
+
+})
---
+++ day-64/app-02.js
@@ -0,0 +1,13 @@
+const invoices =[
+    { no: inv001, client:'abc', amt:1200, dt:'2022-01-01'},
+    { no: inv002, client:'def', amt:1500, dt:'2022-01-02'},
+    { no: inv003, client:'ghi', amt:1800, dt:'2022-01-03'},
+    { no: inv004, client:'jkl', amt:2000, dt:'2022-01-04'},
+    { no: inv005, client:'mno', amt:2200, dt:'2022-01-05'},
+    { no: inv006, client:'pqr', amt:2500, dt:'2022-01-06'},
+    { no: inv007, client:'stu', amt:2700, dt:'2022-01-07'},
+    { no: inv008, client:'vwx', amt:3000, dt:'2022-01-08'},
+    { no: inv009, client:'yza', amt:3200, dt:'2022-01-09'},
+    { no: inv010, client:'bcd', amt:3500, dt:'2022-01-10'}
+];
+console.log(invoices);
