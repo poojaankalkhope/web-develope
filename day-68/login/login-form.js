d62303fc5e1f836bb9f4c777c658fa76d2ba046f
Sumit Parathnale <parathnalesumit@gmail.com>
2024-04-19 15:08:54 +0530

    bcm-basic project

---
+++ day-68/login/login-form.js
@@ -0,0 +1,3 @@
+export function handleForm(e) {
+    e.preventDefault()
+}
---
+++ day-68/login/login.js
@@ -0,0 +1,6 @@
+import {handleForm} from './login-form.js'
+
+const lgnFrm = document.getElementById('lgnFrm')
+lgnFrm = e => {
+    e.preventDefault()
+}
