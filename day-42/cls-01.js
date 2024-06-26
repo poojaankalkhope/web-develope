b7605e7cf812bfc08ccedd1c546339ca1020db1f
Sumit Parathnale <parathnalesumit@gmail.com>
2024-06-26 13:06:03 +0530

    javascript: classes & object

---
+++ day-42/cls-01.js
@@ -0,0 +1,21 @@
+class Mobile {
+
+    constructor (num, cc){
+        this.num = num; // state -> field -> property
+        this.cc = cc;
+    }
+
+      // Note: there no `function` keyword before dial
+    dial() { // behaviour -> function -> method
+        console.log(`Dialing from ${this.cc} country`)
+    }
+
+    disconnect() {
+
+    }
+}
+const mob = new Mobile('123','in');
+console.log(mob.cc);
+console.log(mob.num);
+mob.dial();
+mob.disconnect();
