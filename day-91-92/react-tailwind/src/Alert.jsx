f75e04f00726e970a2b8b25a3a1d1ea46acd2926
Sumit Parathnale <parathnalesumit@gmail.com>
2024-07-24 15:38:47 +0530

    react : component and jsx

---
+++ day-91-92/react-tailwind/src/Alert.jsx
@@ -0,0 +1,13 @@
+export const Alert = () => {
+
+
+    const str = 'bg-red-100'
+
+    return (
+        <div className = {'bg-red-100 border border-red-400 text-red-700 px-4 py-4 m-5 rounded'}>
+            
+            hey hi!
+
+        </div>
+    )
+}
---
+++ day-91-92/react-tailwind/src/App.jsx
@@ -0,0 +1,29 @@
+import { Alert } from "./Alert"
+
+ function App() {
+  
+  return(
+    // day-91
+    // <div className="flex justify-center mt-5">
+    //   hey hi
+    // </div>
+
+    // for day-92
+    <div> 
+    <h1> hey hi!</h1>
+    <h1>hey hi</h1>
+
+    <input type="text" className=''/>
+
+    
+      <Alert/> 
+
+    </div>
+
+    
+  )
+}
+
+ export default App
+
+ 
